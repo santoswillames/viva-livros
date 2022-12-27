e0e54f77c8167151aa8e4c01d3c34421287b73c8
Willames Santos <willamessantos@live.com>
2022-12-26 23:27:32 -0300

    Create routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
+import { Outlet } from "react-router-dom";
+
 function App() {
   return (
     <div className="App">
-      <h1>Hello Word!</h1>
+      <Outlet />
     </div>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,24 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Home from "./pages/Home";
+import Book from "./pages/Book";
+import Search from "./pages/Search";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <BrowserRouter>
+      <Routes>
+        <Route element={<App />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/book/:id" element={<Book />} />
+          <Route path="/search" element={<Search />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
+);
