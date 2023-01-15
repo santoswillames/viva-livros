2e1a7ec5f9bec94e7814f4c088b32054d2e5ebca
Willames Santos <willamessantos@live.com>
2023-01-14 23:58:59 -0300

    eddit path logo

--- src/components/Logo/index.jsx
+++ src/components/Logo/index.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 const Logo = () => {
   return (
     <Link to="/">
-      <img src="/public/book-svgrepo-com-colors.svg" alt="icone de um livro" />
+      <img src="/book-svgrepo-com-colors.svg" alt="icone de um livro" />
     </Link>
   );
 };
